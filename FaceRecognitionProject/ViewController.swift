4612afc2bd228f14b00f41d7539098a36845d0b3
Onuralp <xonuraralpx@gmail.com>
2021-10-05 10:50:00 +0300

    Initial Commit

---
+++ FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by Ceren Çapar on 5.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
