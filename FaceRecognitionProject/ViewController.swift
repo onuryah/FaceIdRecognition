853a845b80a959938eb8e5220a58d8b839e2ec7b
Onuralp <xonuraralpx@gmail.com>
2021-10-20 15:33:12 +0300

    Face ID

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    @IBAction func checkButtonClicked(_ sender: Any) {
+        let authContext = LAContext()
+        
+        var error : NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is It Really You") { success, error in
+                if success == true {
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                    
+                }else {
+                    DispatchQueue.main.async {
+                        self.labelField.text = "Error!"
 
+                    }
+                    
+                }
+            }
+        }
+    }
+    
 
 }
 
